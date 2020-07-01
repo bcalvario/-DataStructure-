2f82b889d7414786fe3ec4ca9b3393f3c3f8cb73
bcalvario <bcalvario@ciencias.unam.mx>
2020-07-01 17:33:24 -0500

    practica08

---
+++ practica8/src/main/java/mx/unam/ciencias/edd/Cola.java
@@ -0,0 +1,39 @@
+package mx.unam.ciencias.edd;
+
+/**
+ * Clase para colas genéricas.
+ */
+public class Cola<T> extends MeteSaca<T> {
+
+    /**
+     * Regresa una representación en cadena de la cola.
+     * @return una representación en cadena de la cola.
+     */
+    @Override public String toString() {
+        Nodo copiaCabeza = cabeza;
+        String cadena = "";
+        while (copiaCabeza != null) {
+            cadena += copiaCabeza.elemento + ",";
+            copiaCabeza = copiaCabeza.siguiente;
+        }
+        return cadena;
+    }
+
+    /**
+     * Agrega un elemento al final de la cola.
+     * @param elemento el elemento a agregar.
+     * @throws IllegalArgumentException si <code>elemento</code> es
+     *         <code>null</code>.
+     */
+    @Override public void mete(T elemento) {
+        if (elemento == null)
+            throw new IllegalArgumentException();
+        Nodo nuevo = new Nodo(elemento);
+        if (cabeza == null)
+            cabeza = rabo = nuevo;
+        else {
+            rabo.siguiente = nuevo;
+            rabo = nuevo;
+        }
+    }
+}
